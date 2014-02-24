c5107fc22451e753a9565cdba32a95a0cc9a5063
Sid Ghodke <sid.ghodke@gmail.com>
2014-02-24 14:22:02 -0800

    make websockets portable to heroku

--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -1,4 +1,6 @@
-var socket = io.connect('http://localhost', 
+//var socket = io.connect('http://localhost', 
+
+var socket = io.connect(location.origin.replace(/^http/, 'ws'),
                { 
                  transports:['websocket'] 
                }
